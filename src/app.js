275d7e82eaa627cc1f4d3e6241fc6f4cd89618bd
dmprosper <95431016+dmprosper@users.noreply.github.com>
2022-01-23 15:10:47 -0600

    showing temp, wind

--- src/app.js
+++ src/app.js
@@ -1,9 +1,22 @@
 function displayTemperature(response){
-    console.log(response.data.main.temp);
+    console.log(response.data);
+    let temperatureElement=document.querySelector("#temperature");
+    let cityElement=document.querySelector("#city");
+    let descriptionElement=document.querySelector("#description");
+    let humidityElement=document.querySelector("#humidity");
+    let windElement=document.querySelector("#wind")
+    temperatureElement.innerHTML=Math.round(response.data.main.temp);
+    cityElement.innerHTML=response.data.name;
+    descriptionElement.innerHTML=response.data.weather[0].description;
+    humidityElement.innerHTML=response.data.main.humidity;
+    windElement.innerHTML=Math.round(response.data.wind.speed);
+
+
+
 }
 
 let apiKey="5896d233f9e55e1c655460c4b3724e91";
-let apiUrl="https://api.openweathermap.org/data/2.5/weather?q=Toronto&appid=${apikey}&units=metric";
+let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayTemperature);
 
 
