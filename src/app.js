33812a9ff3075d6ded607d08dfd53bd4064910af
dmprosper <95431016+dmprosper@users.noreply.github.com>
2022-01-26 13:53:30 -0600

    Added forecast integration

--- src/app.js
+++ src/app.js
@@ -14,7 +14,8 @@ function formatDate(timestamp) {
 
  return `${day} ${hours}:${minutes}`;
 }
-function displayForecast() {
+function displayForecast(response) {
+    console.log(response.data.daily);
     let forecastElement= document.querySelector("#forecast");
     let days = ["Wed", "Thurs", "Fri", "Fri" , "Sat", "Sun"];
 
@@ -40,8 +41,15 @@ function displayForecast() {
     });
         forecastHTML = forecastHTML + `</div>`;
         forecastElement.innerHTML = forecastHTML;
-        console.log(forecastHTML);
-   
+        console.log(forecastHTML);   
+}
+
+function getForecast(coordinates) {
+    console.log(coordinates);
+    let apiKey="5896d233f9e55e1c655460c4b3724e91";
+    let apiUrl =` https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+    console.log(apiUrl);
+    axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemperature(response){
@@ -63,6 +71,9 @@ function displayTemperature(response){
     dateElement.innerHTML = formatDate(response.data.dt * 1000);
     iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
     iconElement.setAttribute("alt", response.data.weather[0].description);
+
+    getForecast(response.data.coord); 
+   
 }
 function search(city) {
   let apiKey="5896d233f9e55e1c655460c4b3724e91";
@@ -112,4 +123,3 @@ celsiusLink.addEventListener("click", displaycelsiusTemperature);
 
 
 search("New York");
-displayForecast();
