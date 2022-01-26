e8b01e5ec088c77ec35a4d43025e540bd2f0da24
dmprosper <95431016+dmprosper@users.noreply.github.com>
2022-01-26 11:38:54 -0600

    HTML & CSS forecast corrected

--- src/app.js
+++ src/app.js
@@ -14,8 +14,35 @@ function formatDate(timestamp) {
 
  return `${day} ${hours}:${minutes}`;
 }
+function displayForecast() {
+    let forecastElement= document.querySelector("#forecast");
+    let days = ["Wed", "Thurs", "Fri", "Fri" , "Sat", "Sun"];
 
-
+    let forecastHTML = `<div class="row">`;
+    
+    days.forEach(function(day) { 
+        forecastHTML = 
+        forecastHTML + 
+        `    
+        <div class="col-2">
+            <div class="weather-forecast-date">${day}</div>                                
+            <img 
+              src="http://openweathermap.org/img/wn/04d@2x.png" 
+              alt="" 
+              width="42" 
+            />
+            <div class="weather-forecast-temperatures">
+                <span class="weather-forecast-temperature-max">18°</span>
+                <span class="weather-forecast-temperature-min">12°</span>
+            </div>                                
+        </div>
+        `;
+    });
+        forecastHTML = forecastHTML + `</div>`;
+        forecastElement.innerHTML = forecastHTML;
+        console.log(forecastHTML);
+   
+}
 
 function displayTemperature(response){
     let temperatureElement=document.querySelector("#temperature");
@@ -85,5 +112,4 @@ celsiusLink.addEventListener("click", displaycelsiusTemperature);
 
 
 search("New York");
-
-
+displayForecast();
